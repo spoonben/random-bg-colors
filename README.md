6010b2eea3a2117e0e8bfdf5fcc34da8164aaa02
Ben Spoon <ben@benspoon.com>
2015-07-17 20:58:10 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+random background colors
+===
+Just a simple snippit that makes some random background spans and inserts them on the page.
+
+Colors come from an array, rotation and number is random. 
---
+++ random-bg-colors.js
@@ -0,0 +1,27 @@
+(function() {
+    var bgImgs = Math.round((Math.random() * 100)),
+        colors = ['#2ecc71', '#3498db', '#f1c40f',
+         '#e74c3c', '#1abc9c', '#9b59b6'],
+         docFrag = document.createDocumentFragment();
+    for(var i=0;i<bgImgs;i+=1) {
+        var span = document.createElement('span'),
+            rotation = Math.round((Math.random() * 360));
+        span.className = 'background-object';
+        span.style.cssText = ['background:' + colors[Math.round((Math.random() * 5))]+';',
+                 '-ms-transform: rotate('+rotation+'deg);',
+                 '-webkit-transform: rotate('+rotation+'deg);',
+                 'transform: rotate('+rotation+'deg);',
+		 'position: absolute;',
+ 		 'overflow: hidden;',
+		 'width:100%;',
+ 		 'height:100%;',
+		 'left:0;',
+		 'top:0;',
+		 'overflow: hidden;',
+		 'z-index: -1;'
+                ].join(' ');
+        docFrag.appendChild(span);
+    }
+    document.body.appendChild(docFrag);
+})();
+
